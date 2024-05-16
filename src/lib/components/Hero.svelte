f825d0ae6ffaef09817e821d0f2666b96a2abb3e
Steven <stevencraig321@gmail.com>
2024-05-16 17:16:50 +0100

    refactor: hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,20 +1,21 @@
 <script>
-	import googlePlay from '$lib/assets/google-play-badge.png'
-	import appStore from '$lib/assets/app_store.svg'
+    import googlePlay from '$lib/assets/google-play-badge.png'
+    import appStore from '$lib/assets/app_store.svg'
+    import hero from '$lib/assets/hero.png'
 </script>
 
-<div
-	class="hero h-[100vh] lg:h-[45rem] shadow-lg"
->
-	<div class="hero-overlay bg-opacity-70" />
-	<div class="hero-content text-neutral-content">
-		<div class="z-60 drop-shadow-lg text-center">
-			<div class="mb-5 text-7xl font-semibold">Turning goals into reality</div>
-			<div class="mb-5 text-7xl font-semibold">One task at a time</div>
-			<div class="flex gap-4 justify-center">
-				<button ><img class="h-16" src={googlePlay} alt={'google play store'}/></button>
-				<button ><img class="h-11" src={appStore} alt={'apple store'}/></button>
-			</div>
+<div class="hero h-[100vh] lg:h-[35rem] shadow-lg flex items-center justify-center">
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content flex-col w-full gap-20 lg:flex-row-reverse">
+		  <img src={hero} class="max-w-md" alt='' />
+		  <div class="text-center">
+            <div class="mb-5 text-5xl font-semibold">Turning goals into reality</div>
+            <div class="mb-5 text-5xl font-semibold">One task at a time</div>
+            <div class="flex gap-4 justify-center pt-10">
+                <button ><img class="h-16" src={googlePlay} alt={'google play store'}/></button>
+                <button ><img class="h-11" src={appStore} alt={'apple store'}/></button>
+            </div>
+		  </div>
 		</div>
-	</div>
+	  </div>
 </div>
