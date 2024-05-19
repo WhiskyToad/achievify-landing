f4bf6f7d03db2af627ec55ec45f0d6f4c42cd3f1
Steven <stevencraig321@gmail.com>
2024-05-19 09:01:09 +0100

    feat: theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,26 @@
 const config = {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#2196f3'
+					// Optionally, you can define different shades of the primary color
+					// For example: 'light': '#your-light-primary-color',
+					//               'dark': '#your-dark-primary-color',
+				},
+				secondary: {
+					DEFAULT: '#ff5722'
+					// Similarly, you can define different shades of the secondary color
+					// For example: 'light': '#your-light-secondary-color',
+					//               'dark': '#your-dark-secondary-color',
+				}
+			}
+		}
 	},
 
-	plugins: [require("daisyui")],
+	plugins: [require('daisyui')]
 };
 
 module.exports = config;
