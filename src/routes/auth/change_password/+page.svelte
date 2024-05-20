8ea6c7fa44d07cae36abbe75c88e2aabdd06e051
Steven <stevencraig321@gmail.com>
2024-05-20 21:52:27 +0100

    refactor: form styling

--- src/routes/auth/change_password/+page.svelte
+++ src/routes/auth/change_password/+page.svelte
@@ -18,28 +18,25 @@
   };
 </script>
 
-<style>
-  .error {
-    color: red;
-  }
-</style>
-
-<form on:submit|preventDefault={handleSubmit}>
-  <label>
-    Password:
-    <input type="password" bind:value={password} />
-  </label>
-  <br />
-
-  <label>
-    Confirm Password:
-    <input type="password" bind:value={confirmPassword} />
-  </label>
-  <br />
-
-  {#if errorMessage}
-    <p class="error">{errorMessage}</p>
-  {/if}
-
-  <button type="submit">Change Password</button>
-</form>
+<section class="bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Reset your password
+                </h1>
+                <form class="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true}>
+                    </div>
+                    <div>
+                        <label for="confirmPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                        <input type="confirmPassword" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true}>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Confirm</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  </section>
