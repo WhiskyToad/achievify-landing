198ba185645341e6c45c5c4c835858b0704fb4c4
Steven <stevencraig321@gmail.com>
2024-05-21 20:59:12 +0100

    feat: password criteria

---
+++ src/lib/components/PasswordCriteria.svelte
@@ -0,0 +1,7 @@
+  <div class="flex items-center gap-4">
+    <svg class="fill-secondary" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+      <path d="M12 22c5.514 0 10-4.486 10-10s-4.486-10-10-10-10 4.486-10 10 4.486 10 10 10zm0-2c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm.75-11.25h-1.5v-5h1.5v5zm-1.5 1.25h1.5v1.5h-1.5v-1.5z"/>
+    </svg>
+    <p>Passwords must be over 8 characters, have a lowercase, an uppercase, and a number.</p>
+  </div>
+  
